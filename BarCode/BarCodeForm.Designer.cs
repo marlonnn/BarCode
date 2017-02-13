06d8506a2a1397bf58793399ec324c90b35ef59c
marlonnn <zhongw_1988@126.com>
2017-02-13 10:24:49 +0800

    1.重命名Form为BarCodeForm;2.添加BarcodeLib项目和引用

--- BarCode/BarCodeForm.Designer.cs
+++ BarCode/BarCodeForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BarCode
 {
-    partial class Form1
+    partial class BarCodeForm
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BarCodeForm));
+            this.SuspendLayout();
+            // 
+            // BarCodeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(533, 476);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BarCodeForm";
+            this.Text = "BarCode";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- BarCode/BarCodeForm.cs
+++ BarCode/BarCodeForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BarCode
 {
-    public partial class Form1 : Form
+    public partial class BarCodeForm : Form
     {
-        public Form1()
+        public BarCodeForm()
         {
             InitializeComponent();
         }
