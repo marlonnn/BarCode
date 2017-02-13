e1b5b589a371cc23f6de263e8501b7d4b6c54940
marlonnn <zhongw_1988@126.com>
2017-02-13 15:42:48 +0800

    1.添加zxing.dll；2.完成条形码基本的生成和识别功能

--- BarCode/BarCodeForm.Designer.cs
+++ BarCode/BarCodeForm.Designer.cs
@@ -29,21 +29,216 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BarCodeForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnDecodeBarCode = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEncodeBarCode = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbEncodeType = new System.Windows.Forms.ComboBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.barcode = new System.Windows.Forms.GroupBox();
+            this.panel1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.groupBox4);
+            this.panel1.Controls.Add(this.btnDecodeBarCode);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnEncodeBarCode);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.cbEncodeType);
+            this.panel1.Controls.Add(this.txtData);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(6, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(269, 467);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtWidth);
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Controls.Add(this.txtHeight);
+            this.groupBox4.Controls.Add(this.label9);
+            this.groupBox4.Location = new System.Drawing.Point(138, 60);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(110, 50);
+            this.groupBox4.TabIndex = 79;
+            this.groupBox4.TabStop = false;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(16, 25);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(35, 20);
+            this.txtWidth.TabIndex = 43;
+            this.txtWidth.Text = "300";
+            this.txtWidth.TextChanged += new System.EventHandler(this.BarWidth_TectChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 41;
+            this.label7.Text = "Width";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 42;
+            this.label6.Text = "Height";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(59, 25);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(35, 20);
+            this.txtHeight.TabIndex = 44;
+            this.txtHeight.Text = "150";
+            this.txtHeight.TextChanged += new System.EventHandler(this.BarHeight_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(50, 27);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(12, 13);
+            this.label9.TabIndex = 51;
+            this.label9.Text = "x";
+            // 
+            // btnDecodeBarCode
+            // 
+            this.btnDecodeBarCode.Location = new System.Drawing.Point(138, 163);
+            this.btnDecodeBarCode.Name = "btnDecodeBarCode";
+            this.btnDecodeBarCode.Size = new System.Drawing.Size(114, 37);
+            this.btnDecodeBarCode.TabIndex = 70;
+            this.btnDecodeBarCode.Text = "&Decode BarCode";
+            this.btnDecodeBarCode.UseVisualStyleBackColor = true;
+            this.btnDecodeBarCode.Click += new System.EventHandler(this.btnDecodeBarCode_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(5, 286);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(247, 36);
+            this.btnSave.TabIndex = 69;
+            this.btnSave.Text = "&Save As Image";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnEncodeBarCode
+            // 
+            this.btnEncodeBarCode.Location = new System.Drawing.Point(5, 163);
+            this.btnEncodeBarCode.Name = "btnEncodeBarCode";
+            this.btnEncodeBarCode.Size = new System.Drawing.Size(114, 37);
+            this.btnEncodeBarCode.TabIndex = 68;
+            this.btnEncodeBarCode.Text = "&Encode BarCode";
+            this.btnEncodeBarCode.UseVisualStyleBackColor = true;
+            this.btnEncodeBarCode.Click += new System.EventHandler(this.btnEncodeBarCode_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 67;
+            this.label3.Text = "Encoding Format";
+            // 
+            // cbEncodeType
+            // 
+            this.cbEncodeType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbEncodeType.FormattingEnabled = true;
+            this.cbEncodeType.ItemHeight = 13;
+            this.cbEncodeType.Location = new System.Drawing.Point(7, 73);
+            this.cbEncodeType.Name = "cbEncodeType";
+            this.cbEncodeType.Size = new System.Drawing.Size(108, 21);
+            this.cbEncodeType.TabIndex = 66;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(5, 25);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(247, 20);
+            this.txtData.TabIndex = 59;
+            this.txtData.Text = "038000356216";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Value to Encode";
+            // 
+            // barcode
+            // 
+            this.barcode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.barcode.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.barcode.Location = new System.Drawing.Point(281, 5);
+            this.barcode.Name = "barcode";
+            this.barcode.Size = new System.Drawing.Size(477, 467);
+            this.barcode.TabIndex = 37;
+            this.barcode.TabStop = false;
+            this.barcode.Text = "Barcode Image";
             // 
             // BarCodeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 476);
+            this.ClientSize = new System.Drawing.Size(763, 476);
+            this.Controls.Add(this.barcode);
+            this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "BarCodeForm";
             this.Text = "BarCode";
+            this.Load += new System.EventHandler(this.BarCodeForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbEncodeType;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEncodeBarCode;
+        private System.Windows.Forms.GroupBox barcode;
+        private System.Windows.Forms.Button btnDecodeBarCode;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- BarCode/BarCodeForm.cs
+++ BarCode/BarCodeForm.cs
@@ -3,18 +3,151 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZXing;
+using ZXing.Common;
 
 namespace BarCode
 {
     public partial class BarCodeForm : Form
     {
+        //条码的宽高
+        private int codeWidth = 300;
+        private int codeHeight = 150;
+
+        private Bitmap barCodeImage = null;
+        private ImageFormat imageformat;
+
         public BarCodeForm()
         {
             InitializeComponent();
+            this.txtWidth.Text = codeWidth.ToString();
+            this.txtHeight.Text = codeHeight.ToString();
+        }
+
+        private void BarCodeForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.cbEncodeType.DataSource = System.Enum.GetNames(typeof(BarcodeFormat));
+                this.cbEncodeType.SelectedIndex = 4;
+            }
+            catch (Exception exception)
+            {
+
+            }
+        }
+
+        //生成条形码
+        private void btnEncodeBarCode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 1.设置条形码规格
+                EncodingOptions encodeOption = new EncodingOptions();
+                encodeOption.Height = codeHeight; // 必须制定高度、宽度
+                encodeOption.Width = codeWidth;
+
+                // 2.生成条形码图片并保存
+                BarcodeWriter wr = new BarcodeWriter();
+                wr.Options = encodeOption;
+                var format = BarcodeFormatHelper.GetFormat(this.cbEncodeType.SelectedItem.ToString());
+                wr.Format = format; //  条形码规格：EAN13规格：12（无校验位）或13位数字
+                barCodeImage = wr.Write(this.txtData.Text); // 生成图片
+                this.barcode.BackgroundImage = barCodeImage;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Exception", MessageBoxButtons.OK);
+            }
+        }
+
+        //修改条形码宽度
+        private void BarWidth_TectChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                this.codeWidth = Int32.Parse(this.txtWidth.Text.ToString());
+            }
+            catch (Exception ee)
+            {
+            }
+        }
+
+        //修改条形码高度
+        private void BarHeight_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                this.codeHeight = Int32.Parse(this.txtHeight.Text.ToString());
+            }
+            catch (Exception ee)
+            {
+            }
+        }
+
+        //读取并解析条形码
+        private void btnDecodeBarCode_Click(object sender, EventArgs e)
+        {
+            if (barCodeImage != null)
+            {
+                // 1.设置读取条形码规格
+                DecodingOptions decodeOption = new DecodingOptions();
+                decodeOption.PossibleFormats = new List<BarcodeFormat>() {
+               BarcodeFormatHelper.GetFormat(this.cbEncodeType.SelectedItem.ToString()),
+            };
+
+                // 2.进行读取操作
+                BarcodeReader br = new BarcodeReader();
+                br.Options = decodeOption;
+                Result rs = br.Decode(barCodeImage);
+                if (rs == null)
+                {
+                    MessageBox.Show("读取失败");
+                }
+                else
+                {
+                    MessageBox.Show("读取成功，内容：" + rs.Text);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (barCodeImage != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "BMP (*.bmp)|*.bmp|GIF (*.gif)|*.gif|JPG (*.jpg)|*.jpg|PNG (*.png)|*.png|TIFF (*.tif)|*.tif";
+                sfd.FilterIndex = 2;
+                sfd.AddExtension = true;
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    switch (sfd.FilterIndex)
+                    {
+                        case 1: /* BMP */
+                            imageformat = System.Drawing.Imaging.ImageFormat.Bmp;
+                            break;
+                        case 2: /* GIF */
+                            imageformat = System.Drawing.Imaging.ImageFormat.Gif;
+                            break;
+                        case 3: /* JPG */
+                            imageformat = System.Drawing.Imaging.ImageFormat.Jpeg;
+                            break;
+                        case 4: /* PNG */
+                            imageformat = System.Drawing.Imaging.ImageFormat.Png;
+                            break;
+                        case 5: /* TIFF */
+                            imageformat = System.Drawing.Imaging.ImageFormat.Tiff;
+                            break;
+                        default: break;
+                    }
+                    barCodeImage.Save(sfd.FileName, imageformat);
+                }
+            }
         }
     }
 }
